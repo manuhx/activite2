8d0e48d86961ac37c9ee512e9f8eab34fad99fa7
manuhx <29374548+manuhx@users.noreply.github.com>
2019-10-18 18:32:55 +0200

    Ajout du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { SinglePostComponent } from './post-list/single-post/single-post.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    SinglePostComponent
+    SinglePostComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
