af475f6b4846174c03e788abd96c269386fcbc7f
manuhx <29374548+manuhx@users.noreply.github.com>
2019-10-18 18:24:18 +0200

    Ajout des components et service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { SinglePostComponent } from './post-list/single-post/single-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
