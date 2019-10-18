db600aff31ca8c9e295d6fb82f39bfd3d3104aa8
manuhx <29374548+manuhx@users.noreply.github.com>
2019-10-18 21:52:02 +0200

    Ajout du component post-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { SinglePostComponent } from './post-list/single-post/single-post.component';
 import { HeaderComponent } from './header/header.component';
+import { PostFormComponent } from './post-list/post-form/post-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const appRoutes: Routes = [
+  {path: 'posts', component: PostListComponent},
+  {path: 'new', component: PostFormComponent},
+  {path: 'post/:id', component: SinglePostComponent},
+  {path: '', redirectTo: 'posts', pathMatch: 'full'},
+  {path: '**', redirectTo: 'posts'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     SinglePostComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
